5558afa3eec40ed82b82c26b0f2656302de8cf3a
darawl <dylanjay9@gmail.com>
2016-05-31 00:02:34 -0700

    Telekinesis Start

--- Assets/Scripts/Telekinesis.cs
+++ Assets/Scripts/Telekinesis.cs
@@ -3,13 +3,72 @@ using System.Collections;
 
 public class Telekinesis : MonoBehaviour {
 
-	// Use this for initialization
+    float lerpTime;
+    float currentLerpTime;
+    bool pull;
+    GameObject moveTarget;
+    float perc;
+    Transform startPos;
+    bool holding;
+
+    // Use this for initialization
 	void Start () {
-	
+        lerpTime = 1f;
+        currentLerpTime = 0f;
+        pull = false;
+        holding = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+        RaycastHit hit;
+        Transform cam = this.transform;
+      
+        if (currentLerpTime <= lerpTime)
+        {
+            currentLerpTime += Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            currentLerpTime = 0f;
+            if(Physics.SphereCast(cam.position, 3, cam.forward, out hit, 500))
+            {
+                //Destroy(hit.transform.gameObject);
+                GameObject target = hit.transform.gameObject;
+                moveTarget = target;
+                if(target.GetComponent("TelikineticObject"))
+                {
+                    pull = true;
+                    startPos = hit.transform;
+                }
+            }
+        }
+
+        if(pull)
+        {
+            perc = currentLerpTime / lerpTime;
+            moveTarget.transform.position = Vector3.Lerp(startPos.position, cam.position + cam.forward * 3, perc);
+            if(currentLerpTime >= lerpTime)
+            {
+                pull = false;
+                holding = true;
+            }
+        }
+
+        if(holding)
+        {
+            moveTarget.transform.position = cam.position + cam.forward * 3;
+            if(Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                holding = false;
+                moveTarget.transform.position = cam.position + cam.forward * 3 + Vector3.up * 1;
+            }
+
+            if (moveTarget.GetComponent<TelikineticObject>().isColliding)
+            {
+                holding = false;
+            }
+        }
 	}
 }
---
+++ Assets/Scripts/TelikineticObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TelikineticObject : MonoBehaviour {
+
+    public bool isColliding;
+
+	// Use this for initialization
+	void Start () {
+        isColliding = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(isColliding);
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        isColliding = true;
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        isColliding = false;
+    }
+}
